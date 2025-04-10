677722910d65e3498c45898a0ce0764c1722fa69
Bina <benjaminbinyam500@gmail.com>
2025-04-10 10:05:08 +0300

    Union type and Literal value type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -14,3 +14,37 @@ let isAdult: boolean = age >= 18;
 isAdult = !isAdult;
 
 console.log(isAdult);
+
+// Union type
+let tax: number | string = 10;
+tax = 10;
+tax = "this is string";
+
+console.log(tax);
+
+// literal value type
+let requestStatus: "pending" | "success" | "error";
+requestStatus = "error";
+requestStatus = "success";
+// requestStatus = "random";
+console.log(requestStatus);
+
+// the type any
+let anyType;
+anyType = 10;
+anyType = "This is a string";
+console.log(anyType);
+
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+
+let foundBook: string | undefined;
+
+for (const book of books) {
+  if (book === "1983") {
+    foundBook = book;
+    foundBook = foundBook.toUpperCase();
+    break;
+  }
+}
+
+console.log(foundBook?.length);
