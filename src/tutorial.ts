3a897b71cd69e6d6c52951d3d3621154d4fff461
Bina <benjaminbinyam500@gmail.com>
2025-04-19 10:30:37 +0300

    Interface - Fundamentals

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,85 +1,69 @@
-// Type Alias => make it capitalize it is a convention
-type User = { id: number; name: string; isActive: boolean };
-const john: User = {
+// Interface
+interface Book {
+  isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+  // methods
+  printAuthor(): void;
+  printTitle(message: string): string;
+  // arrow functions
+  printSomething: (someValue: number) => number;
+}
+
+const deepWork: Book = {
+  isbn: 12345654,
+  title: "Deep Work",
+  author: "Cal Newport",
+  genre: "Self-help",
+  // methods
+  printAuthor() {
+    console.log(`${this.author}`);
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`;
+  },
+  // first option
+  // printSomething: function (someValue) {
+  //   return someValue;
+  // },
+  // second option
+  printSomething: (someValue) => {
+    // console.log(this.author); ERROR: this gotcha
+    console.log(deepWork.author);
+    return someValue;
+  },
+  // third option
+  // printSomething(someValue) {
+  //   return someValue;
+  // },
+};
+
+deepWork.printAuthor();
+const result = deepWork.printTitle("is awesome book.");
+console.log(result);
+
+console.log(deepWork.printSomething(785));
+
+// Challenge
+interface Computer {
+  readonly id: number;
+  brand: string;
+  ram: number;
+  storage?: number;
+  upgradeRam(increase: number): number;
+}
+
+const laptop: Computer = {
   id: 1,
-  name: "john",
-  isActive: true,
-};
-const susan: User = {
-  id: 1,
-  name: "susan",
-  isActive: false,
-};
-
-function createUser(user: User): {
-  id: number;
-  name: string;
-  isActive: boolean;
-} {
-  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
-
-  return user;
-}
-
-type StringOrNumber = string | number;
-let value: StringOrNumber;
-value = "hello";
-value = 89;
-
-type Theme = "light" | "dark";
-let theme: Theme;
-theme = "dark";
-theme = "light";
-
-function setTheme(t: Theme) {
-  theme = t;
-}
-
-setTheme("dark");
-
-// challenge
-type Employee = { id: number; name: string; department: string };
-type Manager = { id: number; name: string; employees: Employee[] };
-
-type Staff = Employee | Manager;
-
-function printStaffDetails(staff: Staff) {
-  if ("employees" in staff) {
-    console.log(
-      `${staff.name} is manager of ${staff.employees.length} employees`
-    );
-  } else {
-    console.log(`${staff.name} is an employee in the ${staff.department}`);
-  }
-}
-
-const alice: Employee = { id: 1, name: "Alice", department: "Sales" };
-const steve: Employee = { id: 1, name: "Steve", department: "HR" };
-const bob: Manager = { id: 2, name: "Bob", employees: [alice, steve] };
-
-printStaffDetails(alice);
-printStaffDetails(bob);
-
-//  Intersection Types
-
-type Book = { id: number; name: string; price: number };
-type DiscountedBook = Book & { discount: number };
-
-const book1: Book = {
-  id: 2,
-  name: "How to Cook a Dragon",
-  price: 15,
+  brand: "radom brand",
+  ram: 8,
+  upgradeRam(amount: number) {
+    this.ram += amount;
+    return this.ram;
+  },
 };
 
-const book2: Book = {
-  id: 3,
-  name: "The Secret Life of Unicorns",
-  price: 18,
-};
-
-const discountedBook: DiscountedBook /*& { discount: number }*/ = {
-  id: 4,
-  name: "Gnomes vs. Goblins: The Ultimate Guide",
-  price: 25,
-  discount: 0.15,
-};
+laptop.storage = 256;
+console.log(laptop.upgradeRam(4));
+console.log(laptop);
