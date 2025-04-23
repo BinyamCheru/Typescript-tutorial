2ff1714cd904294ca0df6286d58b8b6f91924f53
Bina <benjaminbinyam500@gmail.com>
2025-04-23 20:39:24 +0300

    Type Guarding =>'Truthy'/'Falsy' guard

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -40,3 +40,18 @@ function makeSound(animal: Animal) {
     animal.meow();
   }
 }
+
+// METHOD III
+function printLength(str: string | null | undefined) {
+  if (str) {
+    // In this block, TypeScript knows that `str` is a string
+    // because `null` and `undefined` are falsy values.
+    console.log(str.length);
+  } else {
+    console.log('No string provided');
+  }
+}
+
+printLength('Hello'); // Outputs: 5
+printLength(null); // Outputs: No string provided
+printLength(undefined); // Outputs: No string provided
